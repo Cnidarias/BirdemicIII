071cdd2cbc2b7781ed2398ce6b4038eb3e950ff1
Pascal Roessner <pythonmolurus95@gmail.com>
2014-09-05 21:27:36 -0600

    Made things Objects!

---
+++ BirdemicIII/BirdemicIII/Bird.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace BirdemicIII
+{
+    public class Bird : DrawableGameComponent
+    {
+
+        enum CollisionType { None, Building, Boundary, Target }
+        float gameSpeed = 1.0f;
+        struct Bullet
+        {
+            public Vector3 position;
+            public Quaternion rotation;
+        }
+
+        Vector3 lightDirection = new Vector3(3, -2, 5);
+        List<Bullet> bulletList = new List<Bullet>();
+        double lastBulletTime = 0;
+        Model xwingModel;
+        Vector3 xwingPosition = new Vector3(8, 1, -3);
+        public Vector3 Position
+        {
+            get { return xwingPosition; }
+        }
+        Quaternion xwingRotation = Quaternion.Identity;
+        Effect effect;
+        Texture2D bulletTexture;
+
+        //Constructor
+        public Bird(Game game)
+            : base(game)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            lightDirection.Normalize();
+            
+            effect = Game.Content.Load<Effect>("effects");
+            bulletTexture = Game.Content.Load<Texture2D>("bullet");
+            xwingModel = LoadModel("xwing");
+            
+            base.LoadContent();
+        }
+        public override void Update(GameTime gameTime)
+        {
+            float moveSpeed = gameTime.ElapsedGameTime.Milliseconds / 500.0f * gameSpeed;
+            MoveForward(ref xwingPosition, xwingRotation, moveSpeed);
+            ProcessKeyboard(gameTime);
+
+            BoundingSphere xwingSpere = new BoundingSphere(xwingPosition, 0.04f);
+            if (CheckCollision(xwingSpere) != CollisionType.None)
+            {
+                xwingPosition = new Vector3(8, 1, -3);
+                xwingRotation = Quaternion.Identity;
+                gameSpeed /= 1.1f;
+            }
+
+
+            UpdateCamera();
+            UpdateBulletPositions(moveSpeed);
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            DrawModel();
+            DrawBullets();
+            base.Draw(gameTime);
+        }
+
+
+        private void UpdateBulletPositions(float moveSpeed)
+        {
+            for (int i = 0; i < bulletList.Count; i++)
+            {
+                Bullet currentBullet = bulletList[i];
+                MoveForward(ref currentBullet.position, currentBullet.rotation, moveSpeed * 2.0f);
+                bulletList[i] = currentBullet;
+
+                BoundingSphere bulletSphere = new BoundingSphere(currentBullet.position, 0.05f);
+                CollisionType colType = CheckCollision(bulletSphere);
+                if (colType != CollisionType.None)
+                {
+                    bulletList.RemoveAt(i);
+                    i--;
+
+                    if (colType == CollisionType.Target)
+                        gameSpeed *= 1.05f;
+                }
+            }
+        }
+
+        private void UpdateCamera()
+        {
+
+            ((Game1)Game).cameraRotation = Quaternion.Lerp(((Game1)Game).cameraRotation, xwingRotation, 0.1f);
+
+            Vector3 campos = new Vector3(0, 0.1f, 0.6f);
+            campos = Vector3.Transform(campos, Matrix.CreateFromQuaternion(((Game1)Game).cameraRotation));
+            campos += xwingPosition;
+
+            Vector3 camup = new Vector3(0, 1, 0);
+            camup = Vector3.Transform(camup, Matrix.CreateFromQuaternion(((Game1)Game).cameraRotation));
+
+            ((Game1)Game).viewMatrix = Matrix.CreateLookAt(campos, xwingPosition, camup);
+            ((Game1)Game).projectionMatrix = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, Game.GraphicsDevice.Viewport.AspectRatio, 0.2f, 500.0f);
+
+            ((Game1)Game).cameraPosition = campos;
+            ((Game1)Game).cameraUpDirection = camup;
+        }
+
+        private CollisionType CheckCollision(BoundingSphere sphere)
+        {
+            for (int i = 0; i < ((Game1)Game).Env.BuildingBoundingBoxes.Length; i++)
+                if (((Game1)Game).Env.BuildingBoundingBoxes[i].Contains(sphere) != ContainmentType.Disjoint)
+                    return CollisionType.Building;
+
+            if (((Game1)Game).Env.CompleteCityBox.Contains(sphere) != ContainmentType.Contains)
+                return CollisionType.Boundary;
+
+           
+            return CollisionType.None;
+        }
+
+        private void ProcessKeyboard(GameTime gameTime)
+        {
+            float leftRightRot = 0;
+
+            float turningSpeed = (float)gameTime.ElapsedGameTime.TotalMilliseconds / 1000.0f;
+            turningSpeed *= 1.6f * gameSpeed;
+            KeyboardState keys = Keyboard.GetState();
+            if (keys.IsKeyDown(Keys.Right))
+                leftRightRot += turningSpeed;
+            if (keys.IsKeyDown(Keys.Left))
+                leftRightRot -= turningSpeed;
+
+            float upDownRot = 0;
+            if (keys.IsKeyDown(Keys.Down))
+                upDownRot += turningSpeed;
+            if (keys.IsKeyDown(Keys.Up))
+                upDownRot -= turningSpeed;
+
+            Quaternion additionalRot = Quaternion.CreateFromAxisAngle(new Vector3(0, 0, -1), leftRightRot) * Quaternion.CreateFromAxisAngle(new Vector3(1, 0, 0), upDownRot);
+            xwingRotation *= additionalRot;
+
+            if (keys.IsKeyDown(Keys.Space))
+            {
+                double currentTime = gameTime.TotalGameTime.TotalMilliseconds;
+                if (currentTime - lastBulletTime > 100)
+                {
+                    Bullet newBullet = new Bullet();
+                    newBullet.position = xwingPosition;
+                    newBullet.rotation = xwingRotation;
+                    bulletList.Add(newBullet);
+
+                    lastBulletTime = currentTime;
+                }
+            }
+        }
+
+        private Model LoadModel(string assetName)
+        {
+
+            Model newModel = Game.Content.Load<Model>(assetName); foreach (ModelMesh mesh in newModel.Meshes)
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                    meshPart.Effect = effect.Clone();
+            return newModel;
+        }
+
+        private void MoveForward(ref Vector3 position, Quaternion rotationQuat, float speed)
+        {
+            Vector3 addVector = Vector3.Transform(new Vector3(0, 0, -1), rotationQuat);
+            position += addVector * speed;
+        }
+
+        private void DrawModel()
+        {
+            Matrix worldMatrix = Matrix.CreateScale(0.0005f, 0.0005f, 0.0005f) * Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateFromQuaternion(xwingRotation) * Matrix.CreateTranslation(xwingPosition);
+
+            Matrix[] xwingTransforms = new Matrix[xwingModel.Bones.Count];
+            xwingModel.CopyAbsoluteBoneTransformsTo(xwingTransforms);
+            foreach (ModelMesh mesh in xwingModel.Meshes)
+            {
+                foreach (Effect currentEffect in mesh.Effects)
+                {
+                    currentEffect.CurrentTechnique = currentEffect.Techniques["Colored"];
+                    currentEffect.Parameters["xWorld"].SetValue(xwingTransforms[mesh.ParentBone.Index] * worldMatrix);
+                    currentEffect.Parameters["xView"].SetValue(((Game1)Game).viewMatrix);
+                    currentEffect.Parameters["xProjection"].SetValue(((Game1)Game).projectionMatrix);
+                    currentEffect.Parameters["xEnableLighting"].SetValue(true);
+                    currentEffect.Parameters["xLightDirection"].SetValue(lightDirection);
+                    currentEffect.Parameters["xAmbient"].SetValue(0.5f);
+                }
+                mesh.Draw();
+            }
+        }
+
+        private void DrawBullets()
+        {
+            if (bulletList.Count > 0)
+            {
+                VertexPositionTexture[] bulletVertices = new VertexPositionTexture[bulletList.Count * 6];
+                int i = 0;
+                foreach (Bullet currentBullet in bulletList)
+                {
+                    Vector3 center = currentBullet.position;
+
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(1, 1));
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(0, 0));
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(1, 0));
+
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(1, 1));
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(0, 1));
+                    bulletVertices[i++] = new VertexPositionTexture(center, new Vector2(0, 0));
+                }
+
+                effect.CurrentTechnique = effect.Techniques["PointSprites"];
+                effect.Parameters["xWorld"].SetValue(Matrix.Identity);
+                effect.Parameters["xProjection"].SetValue(((Game1)Game).projectionMatrix);
+                effect.Parameters["xView"].SetValue(((Game1)Game).viewMatrix);
+                effect.Parameters["xCamPos"].SetValue(((Game1)Game).cameraPosition);
+                effect.Parameters["xTexture"].SetValue(bulletTexture);
+                effect.Parameters["xCamUp"].SetValue(((Game1)Game).cameraUpDirection);
+                effect.Parameters["xPointSpriteSize"].SetValue(0.1f);
+
+                Game.GraphicsDevice.BlendState = BlendState.Additive;
+
+                foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                {
+                    pass.Apply();
+                    Game.GraphicsDevice.DrawUserPrimitives(PrimitiveType.TriangleList, bulletVertices, 0, bulletList.Count * 2);
+                }
+
+                Game.GraphicsDevice.BlendState = BlendState.Opaque;
+            }
+        }
+    }
+}
---
+++ BirdemicIII/BirdemicIII/environment.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+ 
+
+namespace BirdemicIII
+{
+    public class environment : DrawableGameComponent
+    {
+        int[,] floorPlan;
+        int[] buildingHeights = new int[] { 0, 2, 2, 6, 5, 4 };
+        BoundingBox[] buildingBoundingBoxes;
+        public BoundingBox[] BuildingBoundingBoxes
+        {
+            get { return buildingBoundingBoxes; }
+        }
+        BoundingBox completeCityBox;
+        public BoundingBox CompleteCityBox
+        {
+            get { return completeCityBox; }
+        }
+        Effect effect;
+        Vector3 lightDirection = new Vector3(3, -2, 5);
+        VertexBuffer cityVertexBuffer;
+        Texture2D sceneryTexture;
+
+        Texture2D[] skyboxTextures;
+        Model skyboxModel;
+
+        public environment(Game game)
+            : base(game)
+        {
+        }
+
+        protected override void LoadContent()
+        {
+            lightDirection.Normalize();
+            sceneryTexture = Game.Content.Load<Texture2D>("texturemap");
+            effect = Game.Content.Load<Effect>("effects");
+            skyboxModel = LoadModel("skybox", out skyboxTextures);
+            LoadFloorPlan();
+            SetUpVertices();
+            SetUpBoundingBoxes();
+            base.LoadContent();
+        }
+
+        private void LoadFloorPlan()
+        {
+            floorPlan = new int[,]
+            {
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,1,1,0,0,0,1,1,0,0,1,0,1},
+                {1,0,0,1,1,0,0,0,1,0,0,0,1,0,1},
+                {1,0,0,0,1,1,0,1,1,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,1,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,1,1,0,0,0,1,0,0,0,0,0,0,1},
+                {1,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+                {1,0,0,0,0,1,0,0,0,1,0,0,0,0,1},
+                {1,0,1,0,0,0,0,0,0,1,0,0,0,0,1},
+                {1,0,1,1,0,0,0,0,1,1,0,0,0,1,1},
+                {1,0,0,0,0,0,0,0,1,1,0,0,0,1,1},
+                {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            };
+
+            Random random = new Random();
+            int differentBuildings = buildingHeights.Length - 1;
+            for (int x = 0; x < floorPlan.GetLength(0); x++)
+                for (int y = 0; y < floorPlan.GetLength(1); y++)
+                    if (floorPlan[x, y] == 1)
+                        floorPlan[x, y] = random.Next(differentBuildings) + 1;
+        }
+        private void SetUpBoundingBoxes()
+        {
+            int cityWidth = floorPlan.GetLength(0);
+            int cityLength = floorPlan.GetLength(1);
+
+
+            List<BoundingBox> bbList = new List<BoundingBox>(); for (int x = 0; x < cityWidth; x++)
+            {
+                for (int z = 0; z < cityLength; z++)
+                {
+                    int buildingType = floorPlan[x, z];
+                    if (buildingType != 0)
+                    {
+                        int buildingHeight = buildingHeights[buildingType];
+                        Vector3[] buildingPoints = new Vector3[2];
+                        buildingPoints[0] = new Vector3(x, 0, -z);
+                        buildingPoints[1] = new Vector3(x + 1, buildingHeight, -z - 1);
+                        BoundingBox buildingBox = BoundingBox.CreateFromPoints(buildingPoints);
+                        bbList.Add(buildingBox);
+                    }
+                }
+            }
+            buildingBoundingBoxes = bbList.ToArray();
+
+            Vector3[] boundaryPoints = new Vector3[2];
+            boundaryPoints[0] = new Vector3(0, 0, 0);
+            boundaryPoints[1] = new Vector3(cityWidth, 20, -cityLength);
+            completeCityBox = BoundingBox.CreateFromPoints(boundaryPoints);
+        }
+        private void SetUpVertices()
+        {
+            int differentBuildings = buildingHeights.Length - 1;
+            float imagesInTexture = 1 + differentBuildings * 2;
+
+            int cityWidth = floorPlan.GetLength(0);
+            int cityLength = floorPlan.GetLength(1);
+
+
+            List<VertexPositionNormalTexture> verticesList = new List<VertexPositionNormalTexture>();
+            for (int x = 0; x < cityWidth; x++)
+            {
+                for (int z = 0; z < cityLength; z++)
+                {
+                    int currentbuilding = floorPlan[x, z];
+
+                    //floor or ceiling
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z), new Vector3(0, 1, 0), new Vector2(currentbuilding * 2 / imagesInTexture, 1)));
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 1, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z), new Vector3(0, 1, 0), new Vector2((currentbuilding * 2 + 1) / imagesInTexture, 1)));
+
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 1, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 1, 0), new Vector2((currentbuilding * 2 + 1) / imagesInTexture, 0)));
+                    verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z), new Vector3(0, 1, 0), new Vector2((currentbuilding * 2 + 1) / imagesInTexture, 1)));
+
+                    if (currentbuilding != 0)
+                    {
+                        //front wall
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, 0, -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 1)));
+
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z - 1), new Vector3(0, 0, -1), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+
+                        //back wall
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, 0, -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z), new Vector3(0, 0, 1), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+
+                        //left wall
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, 0, -z), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, 0, -z - 1), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z - 1), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, buildingHeights[currentbuilding], -z), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x, 0, -z), new Vector3(-1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+
+                        //right wall
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z - 1), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z - 1), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 1)));
+
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z - 1), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2 - 1) / imagesInTexture, 0)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, 0, -z), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 1)));
+                        verticesList.Add(new VertexPositionNormalTexture(new Vector3(x + 1, buildingHeights[currentbuilding], -z), new Vector3(1, 0, 0), new Vector2((currentbuilding * 2) / imagesInTexture, 0)));
+                    }
+                }
+            }
+
+            cityVertexBuffer = new VertexBuffer(Game.GraphicsDevice, VertexPositionNormalTexture.VertexDeclaration, verticesList.Count, BufferUsage.WriteOnly);
+
+            cityVertexBuffer.SetData<VertexPositionNormalTexture>(verticesList.ToArray());
+        }
+        private Model LoadModel(string assetName, out Texture2D[] textures)
+        {
+
+            Model newModel = Game.Content.Load<Model>(assetName);
+            textures = new Texture2D[newModel.Meshes.Count];
+            int i = 0;
+            foreach (ModelMesh mesh in newModel.Meshes)
+                foreach (BasicEffect currentEffect in mesh.Effects)
+                    textures[i++] = currentEffect.Texture;
+
+            foreach (ModelMesh mesh in newModel.Meshes)
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                    meshPart.Effect = effect.Clone();
+
+            return newModel;
+        }
+
+
+        private void DrawCity()
+        {
+            
+            effect.CurrentTechnique = effect.Techniques["Textured"];
+            effect.Parameters["xWorld"].SetValue(Matrix.Identity);
+            effect.Parameters["xView"].SetValue(((Game1)Game).viewMatrix);
+            effect.Parameters["xProjection"].SetValue(((Game1)Game).projectionMatrix);
+            effect.Parameters["xTexture"].SetValue(sceneryTexture);
+            effect.Parameters["xEnableLighting"].SetValue(true);
+            effect.Parameters["xLightDirection"].SetValue(lightDirection);
+            effect.Parameters["xAmbient"].SetValue(0.5f);
+
+            foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+            {
+                pass.Apply();
+                
+                Game.GraphicsDevice.SetVertexBuffer(cityVertexBuffer);
+                Game.GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, cityVertexBuffer.VertexCount / 3);
+            }
+        }
+        private void DrawSkybox()
+        {
+            SamplerState ss = new SamplerState();
+            ss.AddressU = TextureAddressMode.Clamp;
+            ss.AddressV = TextureAddressMode.Clamp;
+            Game.GraphicsDevice.SamplerStates[0] = ss;
+
+            DepthStencilState dss = new DepthStencilState();
+            dss.DepthBufferEnable = false;
+            Game.GraphicsDevice.DepthStencilState = dss;
+
+            Matrix[] skyboxTransforms = new Matrix[skyboxModel.Bones.Count];
+            skyboxModel.CopyAbsoluteBoneTransformsTo(skyboxTransforms);
+            int i = 0;
+            foreach (ModelMesh mesh in skyboxModel.Meshes)
+            {
+                foreach (Effect currentEffect in mesh.Effects)
+                {
+                    Matrix worldMatrix = skyboxTransforms[mesh.ParentBone.Index] * Matrix.CreateTranslation(((Game1)Game).bird.Position);
+                    currentEffect.CurrentTechnique = currentEffect.Techniques["Textured"];
+                    currentEffect.Parameters["xWorld"].SetValue(worldMatrix);
+                    currentEffect.Parameters["xView"].SetValue(((Game1)Game).viewMatrix);
+                    currentEffect.Parameters["xProjection"].SetValue(((Game1)Game).projectionMatrix);
+                    currentEffect.Parameters["xTexture"].SetValue(skyboxTextures[i++]);
+                }
+                mesh.Draw();
+            }
+
+            dss = new DepthStencilState();
+            dss.DepthBufferEnable = true;
+            Game.GraphicsDevice.DepthStencilState = dss;
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            DrawSkybox();
+            DrawCity();
+            
+            base.Draw(gameTime);
+        }
+ 
+    }
+}
